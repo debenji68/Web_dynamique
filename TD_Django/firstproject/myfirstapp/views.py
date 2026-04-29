e7f8a4f5d471ac964c60d29682bab91acbbdc412
debenji68 <deiber.benji@gmail.com>
2026-04-29 09:31:46 +0200

    td

--- TD_Django/firstproject/myfirstapp/views.py
+++ TD_Django/firstproject/myfirstapp/views.py
@@ -5,6 +5,6 @@ def index(request):
     return render(request, 'myfirstapp/index.html')
 def formulaire(requests):
     return render(requests, "myfirstapp/formulaire.html")
-def bonjour(request)
+def bonjour(request):
     nom = request.GET("nom")
     return render(request, "myfirstapp/bonjour.html", {"nom":nom})
