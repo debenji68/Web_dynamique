1a9b6e12ebea86fd52341621ecd78759749d30f3
debenji68 <deiber.benji@gmail.com>
2026-04-29 09:31:25 +0200

    td

--- TD_Django/firstproject/myfirstapp/urls.py
+++ TD_Django/firstproject/myfirstapp/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path("index/", views.index),
-    path("formulaire/",views.formulaire)
+    path("formulaire/",views.formulaire),
     path("bonjour/", views.bonjour)
 ]
