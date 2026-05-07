6563f4ab7f4d3085c0e94d5174440fc0e2133e98
debenji68 <deiber.benji@gmail.com>
2026-05-07 15:38:06 +0200

    td

--- TD_Django/firstproject/firstproject/settings.py
+++ TD_Django/firstproject/firstproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myfirstapp.apps.MyfirstappConfig',
+    'crud.apps.CrudConfig',
 ]
 
 MIDDLEWARE = [
--- TD_Django/firstproject/firstproject/urls.py
+++ TD_Django/firstproject/firstproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myfirstapp/', include("myfirstapp.urls"))
+    path('crud/', include("crud.urls"))
 ]
