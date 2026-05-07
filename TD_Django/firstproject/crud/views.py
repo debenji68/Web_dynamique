b035445d67a4d136c94b8c91a7ff47b92088a9e6
debenji68 <deiber.benji@gmail.com>
2026-05-07 16:08:24 +0200

    td

--- TD_Django/firstproject/crud/views.py
+++ TD_Django/firstproject/crud/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'crud/index.html')
+from .forms import LivreForm
+from . import models
+def ajout(request):
+    if request.method == "POST":  # arrive en cas de retour sur cette page après une saisie invalide on récupère donc les données. Normalement nous ne devrions pas passer par ce chemin la pour le traitement des données
+        form = LivreForm(request)
+        if form.is_valid():  # validation du formulaire.
+            Livre = form.save()  # sauvegarde dans la base
+            return render(request, "bibliotheque/affiche.html", {"Livre": Livre})  #envoie vers une page d'affichage du Livre créé
+        else:
+            return render(request, "bibliotheque/ajout.html", {"form": form})
+    else:
+        form = LivreForm()  # création d'un formulaire vide
+        return render(request, "bibliotheque/ajout.html", {"form": form})
