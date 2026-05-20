459be06a55fa784456f8fb0d94e6ba773137ce78
debanji68 <deiber.benji@gmail.com>
2026-05-20 11:21:12 +0200

    tp

--- TP_Django/Voiture/vroumvroum/urls.py
+++ TP_Django/Voiture/vroumvroum/urls.py
@@ -1,11 +1,17 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('ajout/', views.ajout),
-    path('ajoutmarque/', views.ajoutmarque),
-    path('traitement/',views.traitement),
-    path('affiche/<int:id>/',views.read),
-    path('', views.all),
-    path('updatemarque/<int:id>/', views.traitementupdate),
-    path('deletemarque/<int:id>/', views.delete),
+    path('modele/ajout/', views.ajout),
+    path('modele/traitementmodele/', views.traitementmodele),
+    path('modele/affichemodele/', views.readmodele),
+    path('modele/', views.allmodele),
+    path('modele/updatemodele/<int:id>/', views.traitementupdatemodele),
+    path('modele/deletemodele/<int:id>/', views.deletemodele),
+    path('marque/ajoutmarque/', views.ajoutmarque),
+    path('marque/traitement/',views.traitement),
+    path('marque/affichemarque/<int:id>/',views.read),
+    path('marque/', views.all),
+    path('marque/updatemarque/<int:id>/', views.traitementupdate),
+    path('marque/deletemarque/<int:id>/', views.delete),
+    path('index/', views.index),
     ]
--- TP_Django/Voiture/vroumvroum/views.py
+++ TP_Django/Voiture/vroumvroum/views.py
@@ -6,27 +6,71 @@ from . import models
 
 def ajout(request):
     form = ModeleForm()  # création d'un formulaire vide
-    return render(request, "vroumvroum/ajout.html", {"form": form})
+    return render(request, "vroumvroum/modele/ajout.html", {"form": form})
+
+def traitementmodele(request):
+    lform = ModeleForm(request.POST)
+    if lform.is_valid():
+        Modele = lform.save()
+        return render(request, "vroumvroum/modele/affichemodele.html", {"Modele": Modele})
+    else:
+        return render(request, "vroumvroum/modele/ajout.html", {"form": lform})
+
+def readmodele(request, id):
+    Modele = models.Modele.objects.get(pk=id)  # méthode pour récupérer les données dans la base avec un id donnée
+    return render(request, "vroumvroum/modele/affichemodele.html", {"Modele": Modele})
+
+def allmodele(request):
+    Modele = list(models.Modele.objects.all())
+    return render(request, "vroumvroum/modele/all.html", {"Modele": Modele})
+
+def traitementupdatemodele(request, id):
+    modele = get_object_or_404(models.Modele, pk=id)
+
+    if request.method == "POST":
+        lform = ModeleForm(request.POST, instance=modele)
+        if lform.is_valid():
+            lform.save()
+            return HttpResponseRedirect("/vroumvroum/modele/")
+        else:
+            return render(request, "vroumvroum/modele/updatemodele.html", {"form": lform, "id": id})
+    else:
+        lform = ModeleForm(instance=modele)
+        return render(request, "vroumvroum/modele/updatemodele.html", {"form": lform, "id": id})
+
+def deletemodele(request, id):
+    modele = get_object_or_404(models.Modele, pk=id)
+    modele.delete()
+    return HttpResponseRedirect("/vroumvroum/modele/")
+
+def index(request):
+    return render(request, 'vroumvroum/index.html')
+
+
+
+
+
+
 
 def ajoutmarque(request):
     form = MarqueForm()  # création d'un formulaire vide
-    return render(request, "vroumvroum/ajoutmarque.html", {"form": form})
+    return render(request, "vroumvroum/marque/ajoutmarque.html", {"form": form})
 
 def traitement(request):
     lform = MarqueForm(request.POST)
     if lform.is_valid():
         Marque = lform.save()
-        return render(request, "vroumvroum/affichemarque.html", {"Marque": Marque})
+        return render(request, "vroumvroum/marque/affichemarque.html", {"Marque": Marque})
     else:
-        return render(request, "vroumvroum/ajoutmarque.html", {"form": lform})
+        return render(request, "vroumvroum/marque/ajoutmarque.html", {"form": lform})
 
 def read(request, id):
     Marque = models.Marque.objects.get(pk=id)  # méthode pour récupérer les données dans la base avec un id donnée
-    return render(request, "vroumvroum/affiche.html", {"Marque": Marque})
+    return render(request, "vroumvroum/marque/affichemarque.html", {"Marque": Marque})
 
 def all(request):
     Marque = list(models.Marque.objects.all())
-    return render(request, "vroumvroum/all.html", {"Marque": Marque})
+    return render(request, "vroumvroum/marque/all.html", {"Marque": Marque})
 
 def traitementupdate(request, id):
     marque = get_object_or_404(models.Marque, pk=id)
@@ -35,14 +79,14 @@ def traitementupdate(request, id):
         lform = MarqueForm(request.POST, instance=marque)
         if lform.is_valid():
             lform.save()
-            return HttpResponseRedirect("/vroumvroum/")
+            return HttpResponseRedirect("/vroumvroum/marque/")
         else:
-            return render(request, "vroumvroum/updatemarque.html", {"form": lform, "id": id})
+            return render(request, "vroumvroum/marque/updatemarque.html", {"form": lform, "id": id})
     else:
         lform = MarqueForm(instance=marque)
-        return render(request, "vroumvroum/updatemarque.html", {"form": lform, "id": id})
+        return render(request, "vroumvroum/marque/updatemarque.html", {"form": lform, "id": id})
 
 def delete(request, id):
     marque = get_object_or_404(models.Marque, pk=id)
     marque.delete()
-    return HttpResponseRedirect("/vroumvroum/")
+    return HttpResponseRedirect("/vroumvroum/marque/")
